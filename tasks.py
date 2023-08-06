4ae05d61a3b57434fc51f53a794ab1ac0f82ac07
erik b <67238817+blackowe@users.noreply.github.com>
2023-08-06 02:30:04 -0500

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -16,3 +16,17 @@ days_remain = (n_s - (31536000*years_added)) // 86400
 print(days_remain)
 
 # days until leap day = after 59 days
+
+
+import calendar
+def calc_leap_days(yr):
+    # should replaced .isleap() with custome function
+    start_year = 1970 + 2
+    leap_days = 0
+    while start_year <= yr:
+        if calendar.isleap(start_year):
+            leap_days +=1
+        start_year +=4
+    return leap_days
+    
+print(calc_leap_days(2120))
